10974f85a2f131b6019cc692670ebaa2659723ce
stenobot <andrewmyers@gmail.com>
2015-10-02 15:06:20 -0700

    Built Round Summaries Page UI

--- MahjongScorer/Pages/RoundSummariesPage.xaml.cs
+++ MahjongScorer/Pages/RoundSummariesPage.xaml.cs
@@ -26,8 +26,10 @@ namespace MahjongScorer.Pages
         // holder for local Game object
         private Game game;
 
-        private List<string> RoundNamesList = new List<string>();
-        
+        SolidColorBrush titleTextBrush = Application.Current.Resources["MahjongHeaderBrush"] as SolidColorBrush;
+        FontFamily shuiFont = Application.Current.Resources["ShuiFont"] as FontFamily;
+        GridLength titleRowHeight = (GridLength)Application.Current.Resources["RoundSummariesTitleHeight"];
+
 
         public RoundSummariesPage()
         {
@@ -45,30 +47,54 @@ namespace MahjongScorer.Pages
             if (e.Parameter is Game)
             {
                 game = e.Parameter as Game;
+                RenderRoundSummaries();
             }
 
-            for (var i = 0; i < game.RoundSummaries.Count; i++)
-            {
-                RoundNamesList.Add("Round " + (i + 1));
-            }
-
-            roundSummariesPivot.ItemsSource = game.RoundSummaries;
-
-            RenderRoundSummaries();
+            
             base.OnNavigatedTo(e);
         }
 
         private void RenderRoundSummaries()
         {
-            foreach (string roundSummary in game.RoundSummaries)
-            {
+            int roundCount = 1;
+            int rowIndex = 0;
 
+
+
+            foreach (string summary in game.RoundSummaries)
+            {
+                // create two grid rows for each summary, for title and body
+                RowDefinition rdTitle = new RowDefinition();
+                rdTitle.Height = titleRowHeight;
+                RowDefinition rdBody = new RowDefinition();
+                roundSummariesGrid.RowDefinitions.Insert(rowIndex, rdTitle);
+                roundSummariesGrid.RowDefinitions.Insert(rowIndex + 1, rdBody);
+
+                // create new textblocks for title and body
+                TextBlock tbTitle = new TextBlock();
+                TextBlock tbBody = new TextBlock();
+
+                // set text
+                tbTitle.Text = "ROUND " + roundCount;
+                tbBody.Text = summary;
+
+                //set styling
+                tbTitle.Foreground = titleTextBrush;
+                tbTitle.FontFamily = shuiFont;
+                tbBody.TextWrapping = TextWrapping.WrapWholeWords;
+
+                // add as children to grid
+                roundSummariesGrid.Children.Add(tbTitle);
+                roundSummariesGrid.Children.Add(tbBody);
+
+                //set grid rows
+                Grid.SetRow(tbTitle, rowIndex);
+                Grid.SetRow(tbBody, rowIndex + 1);
+
+                roundCount++;
+                rowIndex = rowIndex + 2;
             }
         }
 
-        private void BackButton_Click(object sender, RoutedEventArgs e)
-        {
-            Frame.Navigate(typeof(GameResultsPage), game);
-        }
     }
 }
