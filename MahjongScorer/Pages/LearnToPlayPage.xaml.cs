0bb01c09c5508e50214f888158a651cfe67f4b67
stenobot <andrewmyers@gmail.com>
2016-01-29 21:56:32 -0800

    Command bar and home button on learn to play page

--- MahjongScorer/Pages/LearnToPlayPage.xaml.cs
+++ MahjongScorer/Pages/LearnToPlayPage.xaml.cs
@@ -1,18 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Core;
+﻿using Windows.UI.Core;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Media.Animation;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -35,6 +24,11 @@ namespace MahjongScorer.Pages
             Windows.ApplicationModel.PackageVersion version = pkg.Id.Version;
             aboutVersionNumberTextBlock.Text += string.Format("{0}.{1}.{2}.{3}",
                 version.Major.ToString(), version.Minor.ToString(), version.Build.ToString(), version.Revision.ToString());
+        }
+
+        private void HomeButton_Click(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(StartPage), new DrillInNavigationTransitionInfo());
         }
     }
 }
